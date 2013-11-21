f64391cb132c1b8a37ef8969230271b2545ed9ba
erlaroche <elaroche@athena.att.net>
2013-11-21 15:31:51 -0600

    Add passing code for score (when a 1 is rolled each time) and rolls methods.

--- bowling_game.rb
+++ bowling_game.rb
@@ -5,11 +5,18 @@ class BowlingGame
     @rolls = []
   end
 
+  # Record a roll in the game.
+  #
+  # pins - The Integer number of pins knocked down in this roll.
+  #
+  # Returns nothing.
   def roll(pins)
+    @rolls.push(pins)
   end
 
+  # Returns the Integer score for this game. Expects to be run after all rolls for the game have been recorded.
   def score
-    0
+    @rolls.reduce(:+)
   end
 
 end
